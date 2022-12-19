93ef57a66ce03c7769ba377e951b56df0a4d1398
gwhsu <JIMMY3252968@gmail.com>
2022-12-19 22:50:41 +0800

    1234

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
   <p>123</p>
   <p>
-    Recommended IDE setup:
+    Recommended IDE setup 1234:
     <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
     +
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
