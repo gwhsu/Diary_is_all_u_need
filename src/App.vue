2bc735d3098f9a378ea9a51b24346b20b31f25be
gwhsu <JIMMY3252968@gmail.com>
2022-12-20 15:17:32 +0800

    1220 1500

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Vue logo" src="./assets/1.jpg" />
   <HelloWorld msg="DcnLab Diary" />
 </template>
 
